a01266f8044011d185408a9ff10a05fe6e46b761
Alessandro Pioggia <alessandro.pioggia2@studio.unibo.it>
2025-12-25 10:11:45 +0100

    chore(expenses): setup tests configuration

---
+++ server/services/expenses/tests/Expenses.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Expenses.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ server/services/expenses/tests/Expenses.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Expenses.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
