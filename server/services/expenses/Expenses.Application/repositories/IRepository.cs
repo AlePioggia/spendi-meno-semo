61ab06f1176748687fa30f234d1eec02ea4c238f
Alessandro Pioggia <alexpioggia@gmail.com>
2026-01-01 01:50:00 +0100

    feat(expenses): create generic repository

---
+++ server/services/expenses/Expenses.Application/repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Expenses.Application.repositories
+{
+    public interface IRepository<T, TKey> where T : class
+    {
+        Task<T?> GetByIdAsync(TKey id);
+        Task<List<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
