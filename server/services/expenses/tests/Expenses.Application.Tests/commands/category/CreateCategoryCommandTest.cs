ad264c6b162874c4ef362d24222a200b900b15bb
Alessandro Pioggia <alexpioggia@gmail.com>
2026-01-03 17:54:24 +0100

    fix(expenses): change and fix import for tests

--- server/services/expenses/tests/Expenses.Application.Tests/commands/category/CreateCategoryCommandTest.cs
+++ server/services/expenses/tests/Expenses.Application.Tests/commands/category/CreateCategoryCommandTest.cs
@@ -1,4 +1,5 @@
 ﻿using Expenses.Application.commands.categories;
+using Expenses.Application.commands.categories.createCategory;
 using Expenses.Application.repositories;
 using Expenses.Domain.Entities;
 using Moq;
