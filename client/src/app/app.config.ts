37f49201df13f454b8e934a041e4b1a438f654b0
Alessandro Pioggia <alexpioggia@gmail.com>
2026-01-17 22:58:32 +0100

    feat(frontend): introduce keycloak to project

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { keycloak } from './services/keycloak.service';
+
+export function initializeKeycloak() {
+  return () =>
+    keycloak.init({
+      onLoad: 'login-required',
+      checkLoginIframe: false,
+      pkceMethod: 'S256'
+    });
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAppInitializer(initializeKeycloak())
   ]
 };
